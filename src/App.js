8718dd99a2b7d0064dcc7aa99eb6c9dc9aa2c5d2
caleyKim <nuna11111@naver.com>
2019-02-25 20:07:20 +0900

    비동기 작업

--- src/App.js
+++ src/App.js
@@ -5,47 +5,36 @@ import Movie from './components/movie/Movie'
 
 
 class App extends Component {
-  state = {
-    greating : 'hello',
-    movies : [
-      {
-        title : "Matrix",
-        poster : 'http://cfile230.uf.daum.net/image/120165194C241D693330B4'
-      },
-      {
-        title : "Full Metal Jacket",
-        poster : 'http://nhtech.co.kr/data/file/b05/mobile-view/wrid492_238330197_bCy24ptN_2133055057C30AFF03.jpg'
-      },
-      {
-        title : "Oldboy",
-        poster : 'http://artinsight.co.kr/n_news/peg/1508/thumb/a4cb4757610239a28cf0cba125b24a69_vpZESkDO68sOb6AbZkW9hFd.jpg'
-      },
-      {
-        title : "Star Wars",
-        poster : 'https://t1.daumcdn.net/cfile/tistory/99A0AA3359930EC621'
-      },
-    ]
-  }
+  state = {};
   componentDidMount(){
-    setTimeout(() => {
-      this.setState({
-        movies : [
-          ...this.state.movies,
-          {
-            title : 'transpotting',
-            poster : 'https://t1.daumcdn.net/cfile/tistory/99A0AA3359930EC621'
-          }
-        ]
-      })
-    },2000)
+    this._getMovies();
+  }
+  _renderMovies =() => {
+    // const movies = this.state.movies.map((item,id) => {
+    //   return <Movie title={item.title} poster={item.large_cover_image} key={id}/>
+    // })
+    const movies = this.state.movies.map(item => {
+      return <Movie title={item.title} poster={item.large_cover_image} key={item.id}/>
+    })
+    return movies
+  }
+
+  _getMovies = async () => {
+    const movies  =  await this._callApi();
+    this.setState({
+      movies 
+    })
+  }
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+    .then( response => response.json())
+    .then( result => result.data.movies)
+    .catch( err => console.log(err) )
   }
   render() {
     return (
       <div className="App">
-        {this.state.greating}
-        {this.state.movies.map((item,id) => {
-          return <Movie title={item.title} poster={item.poster} key={id}/>
-        })}
+        {this.state.movies ? this._renderMovies() : 'loading'}
       </div>
     );
   }
